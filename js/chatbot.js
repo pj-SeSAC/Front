840fcf41dbbdbf445e7b85451a63e4691f1e85a6
soominee2 <mmovie00@daum.net>
2023-12-11 20:05:39 +0900

    chatting_basic

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,3 +1,29 @@
-document.getElementById("Button").onclick = function () {
-    location.href = "main.html"; // "destination.html"은 이동할 페이지의 URL
-};
+// 작성 버튼 눌렀을 떄
+$('.ButtonSend').click(ChatSend);
+
+function ChatSend() {
+        let chatbotSub = {
+            "message": $('.ChatInput').val()
+        };
+        if (chatbotSub.message) {
+            $.ajax({
+                type: 'POST',
+                url: 'http://127.0.0.1:8000/submit',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: JSON.stringify(chatbotSub),
+                success: function (result) {
+                    console.log(result)
+                    $('.chat-container').append('<div class="message-given">' + chatbotSub.message + '</div>');
+                    $('.chat-container').append('<div class="message-received">' + result.response + '</div>');
+                    $('.ChatInput').val(''); 
+                },
+                error: function (xhr, status, error) {
+                    console.error("Error: " + error);
+                    console.error("Status: " + status);
+                    console.error("Response: " + xhr.responseText);
+                }
+            });
+        }
+    };
+
