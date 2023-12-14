d4f8dcbd552361a4b72c3fcbabd38698258e0ad9
soominee2 <mmovie00@daum.net>
2023-12-14 15:31:24 +0900

    선택형 질문

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,5 +1,11 @@
 // 작성 버튼 눌렀을 떄
 $('.ButtonSend').click(ChatSend);
+$('.ChatInput').keypress(function(e) {
+    if (e.which == 13) {
+        ChatSend();
+        e.preventDefault();
+    }
+});
 
 function ChatSend() {
     let chatbotSub = {
@@ -27,6 +33,9 @@ function ChatSend() {
                 console.log(chatbotSub.message)
                 console.log(result)
                 $('.chat-container').append('<div class="message-received">' + result.response + '</div>');
+                $('.chat-container').append('<div class="message-received">궁금하신 사항이 있으면 말해주세요.</div>');
+    
+                displayChoiceButtons();
                 scrollToBottom();
             },
             error: function (xhr, status, error) {
@@ -46,16 +55,16 @@ $(document).ready(function() {
         let responseText = "";
 
         if (buttonText === "ABOUT ME") {
-            responseText = "여기에 'ABOUT ME'에 대한 응답 텍스트를 입력하세요.";
+            responseText = ('1. 나의 장점 3가지 알려줘.\n 2. 내가 보완해야 할 점을 3가지 알려줘.\n3. 나의 가치관에 대해 알려줘.\n4. 내가 최근에 느낀 감정 3가지 알려줘.').replace(/\n/g, '<br>');
         } else if (buttonText === "HOW TO") {
-            responseText = "여기에 'HOW TO'에 대한 응답 텍스트를 입력하세요.";
+            responseText = ('1. 내가 행복해질 수 있는 3가지 방법을 알려줘.\n 2. 내가 스트레스를 해소 할 수 있는 3가지 방법을 알려줘.\n3. 내가 내일 혼자 무엇을 하면 좋을지 알려줘.').replace(/\n/g, '<br>');
         } else if (buttonText === "REMEMBER") {
-            responseText = "여기에 'REMEMBER'에 대한 응답 텍스트를 입력하세요.";
+            responseText = ('1. 내가 행복해질 수 있는 3가지 방법을 알려줘.\n 2. 내가 2023년에 가장 행복했던 날과 이유를 알려줘.\n3. 나는 일요일에 주로 무엇을 하는지 3개만 알려줘.\n  ✩ 이외에도 REMEMBER와 관련한 다양한 질문을 자유롭게 물어보세요!').replace(/\n/g, '<br>');
         }
 
         // 응답과 추가 메시지를 추가합니다.
         $('.chat-container').append('<div class="message-received">' + responseText + '</div>');
-        $('.chat-container').append('<div class="message-received">궁금하신 사항이 있으면 말해주세요.</div>');
+        $('.chat-container').append('<div class="message-received">래미에게 더 궁금하신 사항이 있으면 말해주세요!</div>');
         
 
         displayChoiceButtons();
@@ -63,12 +72,6 @@ $(document).ready(function() {
         scrollToBottom();
         // 버튼을 다시 표시합니다.
         
-        $('.ChatInput').keypress(function(e) {
-            if (e.which == 13) {
-                ChatSend();
-                e.preventDefault();
-            }
-        });
         
     });
 });
